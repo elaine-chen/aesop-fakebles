fbec1fa3429b52e0e7fbc8c6a7271f8eae77147b
Elaine Chen <yxnchen@gmail.com>
2017-11-08 18:22:55 -0800

    removed comment that mention the word flask??

--- aesop.py
+++ aesop.py
@@ -1,14 +1,7 @@
 from parser import Parser
 import gen
-# from flask import Flask
 
 def main(source):
     to_parse = Parser(source, 2)
     freq = to_parse.parse()
     return gen.gen_text(freq, 500)
-
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def gen_story():
-#     return parser_text.main("fab.mb.txt")
