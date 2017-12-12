29617951308e5d9af4b3482ae18f6413cd3498af
Elaine Chen <yxnchen@gmail.com>
2017-12-11 18:48:12 -0800

    fixed format and made gen.py more simple

--- aesop.py
+++ aesop.py
@@ -5,17 +5,12 @@ import grab_titles
 
 def gen_dict(source, k):
     to_parse = Parser(source, k)
-    print "DICTONARY HAS BEEN GENERATED"
     return to_parse.parse()
 
 app = Flask(__name__)
 
 lines = grab_titles.grab("11339-8.txt")
 
-# story_dict = gen_dict("11339-8.txt")
-# story_dict_len = len(story_dict)
-# grab_titles.get_titles()
-# grab_titles.get_morals()
 t = grab_titles.grab_t(lines)
 title_dict = gen_dict(t, 2)
 title_dict_len = len(title_dict)
@@ -26,15 +21,17 @@ text_dict_len = len(text_dict)
 
 m = grab_titles.grab_m(lines)
 morals_dict = gen_dict(m, 1)
-print morals_dict
 morals_dict_len = len(morals_dict)
 
-
+def gen_story():
+    body =  gen.gen_text(text_dict, 500, True, text_dict_len)
+    title = gen.gen_text(title_dict, 10, False, title_dict_len)
+    moral = gen.gen_text(morals_dict, 10, True, morals_dict_len)
+    return title + "\n\n\t" + body + "\n\n\t" + moral
 
 
 @app.route('/')
 def story():
-    body =  gen.gen_text(text_dict, 500, True, text_dict_len)
-    title = gen.gen_text(title_dict, 10, False, title_dict_len) + '\n'
-    moral = '\t' + gen.gen_text(morals_dict, 10, True, morals_dict_len)
-    return moral
+    response = app.make_response(gen_story())
+    response.headers["content-type"] = "text/plain"
+    return response
--- gen.py
+++ gen.py
@@ -5,39 +5,45 @@ import numpy as np
     word that occurs befor every 'the' ?? """
 
 def gen_text(freq_map, length, is_story, map_len):
+    text = ""
     l_dup = length
     body = []
     cur = freq_map.keys()[randint(0,map_len)]
     # print "current word ", cur
-    body.append("   ")
     body.append(cur[0][0].capitalize() + cur[0][1:])
-    body.append(cur[1])
-    while length > 0:
+    for word in cur[1:]:
+        body.append(word)
+    print body
+    not_at_end = True
+    while length > 0 or not_at_end == True:
         next_word = get_random(freq_map[cur])
         body.append(next_word)
         cur = cur[1:] + (next_word,)
         length -= 1
-    if is_story:
-        story = " ".join(body)
-        if story[-1] in [".", "?", "!"]:
-            return story
-        else:
-            last_word_ind = max(story.rfind("."), story.rfind("!"), story.rfind("?"))
-            return story[:last_word_ind+1]
-    else:
-        if body[-1] != "THE" and body[-1] != "AND":
-            title = " ".join(body)
-        else:
-            for i in reversed(xrange(l_dup+1)):
-                if body[i] != "THE" and body[i] != "AND":
-                    body = body[:i+1]
+        if length <= 0:
+            if is_story == True:
+                if body[-1][-1] in [".", "?", "!"]:
+                    not_at_end = False
+                    body.insert(0, "\t")
+                    text = " ".join(body)
                     break
                 else:
                     continue
-            title = " ".join(body)
-        if title[-1] == ",":
-            title = title[:-1]
-        return title
+            else:
+                if body[-1] != "THE" and body[-1] != "AND":
+                    not_at_end = False
+                    if body[0] == "AND":
+                        body = body[1:]
+                    text = " ".join(body)
+                    print body
+                    if text[-1] == ",":
+                        text = text[:-1]
+                    break
+                else:
+                    continue
+        else:
+            continue
+    return text
 
 
 def get_random(subwords):
@@ -45,8 +51,6 @@ def get_random(subwords):
     prob_words = []
     freq_sum = sum(subwords.values())
     for item in subwords:
-        # print "the item ", item
         prob_dist.append((subwords[item]+0.0)/freq_sum)
         prob_words.append(item)
-        # print "Potential words ", prob_words
     return np.random.choice(prob_words, p = prob_dist)
